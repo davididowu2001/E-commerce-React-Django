868e024d671eef0ee316c7709a0fcd65062f972f
davididowu2001 <idowu.subomi9@gmail.com>
2024-09-16 16:19:43 +0100

    adding front end

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+
+function Header() {
+  return (
+    <header>
+         <Navbar bg="dark" variant="dark" collapseOnSelectexpand>
+      <Container>
+        <Navbar.Brand href="/">Tehillim</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>CART</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i>LOGIN</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  
+    </header>
+  )
+}
+
+export default Header
