8c2d9b823fe30a6dc892d5255417101f3555a122
davididowu2001 <idowu.subomi9@gmail.com>
2024-09-20 21:15:54 +0100

    fetched data from db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     
     'base.apps.BaseConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,5 +125,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,15 +3,24 @@ import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
+import axios from 'axios'
 
 function ProductScreen() {
   const { id } = useParams(); // Use useParams hook to get the id parameter
 
-  const product = products.find((p) => p._id === id); // Use the id from useParams
 
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    
+    async function fetchProduct(){
+      const { data } = await axios.get(`/api/products/${id}`)
+     
+      setProduct(data)
+    }
+    fetchProduct()
+    
+  }, [])
 
   return (
     <div>
